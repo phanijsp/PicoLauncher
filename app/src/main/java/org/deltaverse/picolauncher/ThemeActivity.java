87bd257f5186e9298224d7128f7e63bac8727032
phanijsp <phani.jsp@gmail.com>
2020-08-06 00:17:53 +0530

    Implemented a rewarded video ad for changing themes

--- app/src/main/java/org/deltaverse/picolauncher/ThemeActivity.java
+++ app/src/main/java/org/deltaverse/picolauncher/ThemeActivity.java
@@ -1,20 +1,25 @@
 package org.deltaverse.picolauncher;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdCallback;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
 import java.util.ArrayList;
-
-import soup.neumorphism.NeumorphCardView;
 
 public class ThemeActivity extends AppCompatActivity {
 
@@ -23,6 +28,9 @@ public class ThemeActivity extends AppCompatActivity {
 	ImageView backButton;
 	GridAdapter gridAdapter;
 	ThemeUtils themeUtils;
+	RewardedAd rewardedAd;
+	boolean ad_loaded = false;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,9 +48,22 @@ public class ThemeActivity extends AppCompatActivity {
 
 		gridItemClickHandler();
 		backButtonClickHandler();
+		initAd();
 	}
 
-	public void backButtonClickHandler(){
+	public void initAd() {
+		rewardedAd = new RewardedAd(this, "ca-app-pub-2930601886283388/1971312861");
+		RewardedAdLoadCallback adLoadCallback = new RewardedAdLoadCallback() {
+			@Override
+			public void onRewardedAdLoaded() {
+				ad_loaded = true;
+				super.onRewardedAdLoaded();
+			}
+		};
+		rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
+	}
+
+	public void backButtonClickHandler() {
 		backButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -50,16 +71,46 @@ public class ThemeActivity extends AppCompatActivity {
 			}
 		});
 	}
+
 	public void gridItemClickHandler() {
 		gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				gridAdapter.updateTheme(themeObjects.get(position).getTheme());
-				gridAdapter.notifyDataSetChanged();
-				themeUtils.setTheme(themeObjects.get(position).getTheme());
+			public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+				if (ad_loaded) {
+					DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							switch (which) {
+								case DialogInterface.BUTTON_POSITIVE:
+									rewardedAd.show(ThemeActivity.this, new RewardedAdCallback() {
+										@Override
+										public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+											Log.i("Here", "onUserEarnedReward: ");
+											gridAdapter.updateTheme(themeObjects.get(position).getTheme());
+											gridAdapter.notifyDataSetChanged();
+											themeUtils.setTheme(themeObjects.get(position).getTheme());
+										}
+									});
+									break;
+								case DialogInterface.BUTTON_NEGATIVE:
+									break;
+							}
+						}
+					};
+					AlertDialog.Builder builder = new AlertDialog.Builder(ThemeActivity.this);
+					builder.setMessage("Watch Ad to save changes").setPositiveButton("Okay", dialogClickListener)
+							.setNegativeButton("Cancel", dialogClickListener).show();
+				} else {
+					gridAdapter.updateTheme(themeObjects.get(position).getTheme());
+					gridAdapter.notifyDataSetChanged();
+					themeUtils.setTheme(themeObjects.get(position).getTheme());
+				}
+
+
 			}
 		});
 	}
+
 
 	@Override
 	public void onBackPressed() {
